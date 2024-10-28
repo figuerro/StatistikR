0a1f735ca566b1fad58773843a69dd8b7d126756
figuerro <154385963+figuerro@users.noreply.github.com>
2024-10-28 10:44:22 +0100

    Add files via upload

---
+++ Uebung1.R
@@ -0,0 +1,36 @@
+#Übung1 Aufgabe 1 Rstudio
+
+#1
+52.3+74.8+3.17
+
+sqrt(144)
+
+log(200, 10)
+
+log(200, 10) * (pi/4)
+
+cumsum(c(1,3,18,20,2))
+
+sample(0:20, 10, replace = TRUE)
+
+#2
+x <- 5
+
+y <- 10
+
+x = 5
+
+y = 10
+
+
+x + y
+
+z <- x * y
+z
+
+?vector
+myvec <- c(x,y,z)
+min(myvec)
+max(myvec)
+mean(myvec)
+remove(myvec)
